2c329920fa0dcc18ac427e2469ece03706c495e9
morarasu-alexandru <alexandru.morarasu@everymatrix.com>
2019-04-03 07:55:01 +0300

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,26 @@
-async function myFunc() {
-    console.log('Works');
-}
+const variantList = document.getElementById('variantList');
+const Buttonversion1 = document.getElementById('version1');
+const Buttonversion2 = document.getElementById('version2');
 
-myFunc()
-    .then(res => console.log(res));
+Buttonversion1.addEventListener('click', () => {
+  displayOnlyVariant(1);
+});
+
+Buttonversion2.addEventListener('click', () => {
+  displayOnlyVariant(2);
+});
+
+
+const displayOnlyVariant = (variantId) => {
+  let children = [...variantList.children];
+
+  children.forEach((elem, index) => {
+    if (!elem.classList.contains('noDisplay' || index !== (variantId - 1))) {
+      elem.classList.add('noDisplay');
+    }
+
+    if (index === (variantId - 1)) {
+      elem.classList.remove('noDisplay')
+    }
+  });
+};
